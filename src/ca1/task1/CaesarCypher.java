7259eb0f60ee17b3d7d5b0f61e3a4d16acddbb35
Alan Cheuk <cheac001@mymail.unisa.edu.au>
2022-04-23 16:04:06 +0930

    Create variables and constructors

---
+++ src/ca1/task1/CaesarCypher.java
@@ -0,0 +1,31 @@
+package ca1.task1;
+
+public class CaesarCypher {
+	
+	// Create variables
+	
+	private char[] input = {};
+	private char[] afterEncode = new char[input.length];
+	private int offset = 0;
+	
+	char[] result = {};
+	
+	
+	// Create constructor for variables
+	
+	public CaesarCypher(char[] input, int offset) {
+		
+		this.input = input;
+		this.offset = offset;
+		
+	}
+	
+
+	public char[] encode(char[] input, int offset) {
+		return null;
+	}
+	
+	public char[] decode(char[] input, int offset) {
+		return null;
+	}
+}
