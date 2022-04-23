07e85e2ac4eb3d5cc00d5d2171d2de20ed89c6a0
Alan Cheuk <cheac001@mymail.unisa.edu.au>
2022-04-23 17:09:34 +0930

    Create method to encode input array

--- src/ca1/task1/CaesarCypher.java
+++ src/ca1/task1/CaesarCypher.java
@@ -6,6 +6,7 @@ public class CaesarCypher {
 	
 	private char[] input = {};
 	private char[] afterEncode = new char[input.length];
+	private char[] afterDecode = new char[input.length];
 	private int offset = 0;
 	
 	char[] result = {};
@@ -20,12 +21,63 @@ public class CaesarCypher {
 		
 	}
 	
+	// Create method to encode input array
+	
 
-	public char[] encode(char[] input, int offset) {
-		return null;
+	public char[] encode() {
+		
+		// Create local variables
+		
+		int i;
+		int asciiCode;
+		char character;
+		char[] afterEncode = new char[input.length];
+		
+		char[] result = {};
+		
+		// Use for loop to convert character of array to ascii code and add offset number before convert back to character
+		
+		for (i = 0; i < input.length; i++) {
+			
+			character = (input[i]);
+			asciiCode = character;
+			afterEncode[i] = (char)(asciiCode + offset);
+				
+		}
+		
+		// Output decoded array
+		
+		result = afterEncode;
+
+		return afterEncode;
 	}
 	
 	public char[] decode(char[] input, int offset) {
 		return null;
 	}
+	
+	
+	public static void main(String[] arg) {
+		
+		char test = 'a';
+		char test1;
+		
+		int number = test;
+		test1 = (char) (number +1);
+		
+		System.out.println(number );
+		System.out.println(test1);
+		
+		char[] input = {'a', 'b', 'c'};
+		
+		CaesarCypher cc = new CaesarCypher(input, 2);
+		
+		System.out.println(cc.encode());
+		
+		
+		
+		
+		
+	}
+	
 }
