d9114dee03574c3ade38b72a866bd5c272487e50
Alan Cheuk <cheac001@mymail.unisa.edu.au>
2022-04-23 17:29:15 +0930

    Create method to decode encoded array

--- src/ca1/task1/CaesarCypher.java
+++ src/ca1/task1/CaesarCypher.java
@@ -1,83 +1,103 @@
 package ca1.task1;
 
 public class CaesarCypher {
-	
+
 	// Create variables
-	
+
 	private char[] input = {};
-	private char[] afterEncode = new char[input.length];
-	private char[] afterDecode = new char[input.length];
+
 	private int offset = 0;
-	
+
 	char[] result = {};
-	
-	
+
 	// Create constructor for variables
-	
+
 	public CaesarCypher(char[] input, int offset) {
-		
+
 		this.input = input;
 		this.offset = offset;
-		
+
 	}
-	
+
 	// Create method to encode input array
-	
 
 	public char[] encode() {
-		
+
 		// Create local variables
-		
+
 		int i;
 		int asciiCode;
 		char character;
 		char[] afterEncode = new char[input.length];
-		
-		char[] result = {};
-		
+
+		//char[] result = {};
+
 		// Use for loop to convert character of array to ascii code and add offset number before convert back to character
-		
+
 		for (i = 0; i < input.length; i++) {
-			
+
 			character = (input[i]);
 			asciiCode = character;
-			afterEncode[i] = (char)(asciiCode + offset);
-				
+			afterEncode[i] = (char) (asciiCode + offset);
+
 		}
-		
-		// Output decoded array
-		
+
+		// Output encoded array
+
 		result = afterEncode;
 
 		return afterEncode;
 	}
-	
-	public char[] decode(char[] input, int offset) {
-		return null;
-	}
-	
-	
-	public static void main(String[] arg) {
+
+
+	// Create method to decode encoded array
+
+	public char[] decode() {
+
+		// Create local variables
+
+		int i;
+		int asciiCode;
+		char character;
+		char[] afterDecode = new char[input.length];
+
+
+		// Use for loop to convert character of decoded array to ascii code and subtract offset number before convert back to character
 		
+
+		for (i = 0; i < input.length; i++) {
+
+			character = (result[i]);
+			
+			asciiCode = character;
+			
+			afterDecode[i] = (char) (asciiCode - offset);
+			
+		}
+		
+		// Output decoded array
+
+		return afterDecode;
+	}
+
+	public static void main(String[] arg) {
+
 		char test = 'a';
 		char test1;
-		
+
 		int number = test;
-		test1 = (char) (number +1);
-		
-		System.out.println(number );
+		test1 = (char) (number + 1);
+
+		System.out.println(number);
 		System.out.println(test1);
-		
-		char[] input = {'a', 'b', 'c'};
-		
+
+		char[] input = { 'a', 'b', 'c' };
+
 		CaesarCypher cc = new CaesarCypher(input, 2);
-		
+
 		System.out.println(cc.encode());
-		
-		
-		
-		
-		
+		System.out.println(cc.decode());
+
 	}
-	
+
 }
