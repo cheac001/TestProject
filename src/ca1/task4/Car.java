5dba2c910a4f0fb27b774f1e01433cbd557f698a
cheac001 <cheac001@mymail.unisa.edu.au>
2022-06-18 16:40:33 +0930

    Creat testing on GitHub

--- src/ca1/task4/Car.java
+++ src/ca1/task4/Car.java
@@ -4,13 +4,13 @@ package ca1.task4;
 
 public class Car {
 
-	// Create variables "make", "model" and "color"
+	// Create three variables "make", "model" and "color"
 
 	private String make = "";
 	private String model = "";
 	private String color = "";
 
-	// Create constructors for variables;
+	// Create constructors for three variables;
 
 	public Car(String make, String model, String color) {
 
@@ -19,25 +19,9 @@ public class Car {
 		this.color = color;
 
 	}
+
 	
-
-
-	// Create method toString to return a string representation of a car
-
-	public String getMake() {
-		return make;
-	}
-
-
-	public String getModel() {
-		return model;
-	}
-
-
-	public String getColor() {
-		return color;
-	}
-
+	// Create a method to return a string representation of a car
 
 	public String toString() {
 
@@ -45,17 +29,22 @@ public class Car {
 
 	}
 
+	
 	// Create method equals to compare model and color between objects
 
 	public boolean equals(Car obj) {
 
-		// use if & else statement to check whether both model and color are the same
-		// between objects and return result true or false
+		// Use equals method in if & else statement to check whether both model and
+		// color are the same between objects and return result true or false
+		
+		// If the same then return true
 
 		if (model == obj.model && color == obj.color) {
 
 			return true;
 		}
+		
+		// If different then return false
 
 		else {
 
@@ -63,16 +52,17 @@ public class Car {
 		}
 	}
 
-	// Create method hashCode to return a integer to be used for comparison between
-	// objects
+	
+	// Create method hashCode to return a integer to be used for comparison between objects
 
 	public int hashCode() {
 
 		int totalHashCode = model.hashCode() + color.hashCode();
 
 		return totalHashCode;
-
 	}
+
+//  Create main method to execute the program
 
 	public static void main(String[] args) {
 
@@ -83,30 +73,5 @@ public class Car {
 		Car car0 = new Car(make, model, color);
 
 		System.out.println(car0.toString());
-
-		Car[] cars = { new Car("Honda", "Civic", "Red"), new Car("Honda", "Civic", "Red"),
-				new Car("Jeep", "Wrangler", "Blue"), new Car("Jeep", "Wrangler", "Green"),
-				new Car("Holden", "Falcon", "Yellow"), new Car("Honda", "Civic", "Blue"),
-				new Car("Honda", "Falcon", "Blue"), new Car("Honda", "Civic", "Purple") };
-
-		int i;
-
-		for (i = 0; i < cars.length; i++) {
-
-			System.out.println(cars[i].toString());
-
-		}
-		
-		Car car1 = new Car("Honda", "Civic", "Red");
-		Car car2 = new Car("Jeep", "Wrangler", "Blue");
-		
-		System.out.println(car0.equals(car1));
-		System.out.println(car0.equals(car2));
-		
-		System.out.println(car0.hashCode());
-		System.out.println(car1.hashCode());
-		System.out.println(car2.hashCode());
-		
 	}
-
 }
--- src/ca1/task4/CarTest.java
+++ src/ca1/task4/CarTest.java
@@ -5,36 +5,36 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
-	
+
 	// Perform first test with input Jeep, Wrangler and Blue
 
 	@Test
 	void testToString1() {
-		
+
 		Car c = new Car("Jeep", "Wrangler", "Blue");
-		
+
 		String expected = "Jeep , Wrangler , Blue";
-		
+
 		assertArrayEquals(expected, c.toString(), "The toString method does not work as expected");
 
 	}
 
 	private void assertArrayEquals(String expected, String string, String string2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	// Perform second test with input Toyota , Camry and Green
-	
+
 	@Test
 	void testToString2() {
-		
+
 		Car c = new Car("Toyota", "Camry", "Green");
-		
+
 		String expected = "Toyota , Camry , Green";
-		
+
 		assertArrayEquals(expected, c.toString(), "The toString method does not work as expected");
 
 	}
-	
+
 }
